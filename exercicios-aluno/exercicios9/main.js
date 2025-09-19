ed360d6b9b1bc1fcc1423fdce9129722fcec9b7e
aneli-hub <oliveiraaninha587@gmail.com>
2025-09-19 20:11:04 -0300

    correção exercício 3 for

--- exercicios-aluno/exercicios9/main.js
+++ exercicios-aluno/exercicios9/main.js
@@ -1,13 +1,13 @@
 //Escreva um programa em que o usuário insira 10 números, exiba a contagem de quantos números inseridos são maiores do que 50;
 
-let numeros = 0;
-let soma;
+let maioresQue50 = 0; // contador de números maiores que 50
 
-for (let contador = 0; contador < 10; contador = contador + 1 ) {
-    soma = prompt("digite 10 numeros");
-    soma = Number (soma);
-    numeros = numeros + soma;
-    
-//falta o if
+for (let i = 1; i <= 10; i++) {
+  let numero = Number(prompt(`Digite o ${i}º número:`));
+  
+  if (numero > 50) {
+    maioresQue50++;
+  }
+}
 
-} console.log('soma dos numeros maiores de 50 ', ());
+console.log(`Foram inseridos ${maioresQue50} números maiores que 50.`);
